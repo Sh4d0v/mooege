bd8f55f0dd8aa331e49afca431362396bd39575e
011082 <korrievan@gmail.com>
2018-10-18 22:57:55 +1000

    Fix Program

--- src/Mooege/Program.cs
+++ src/Mooege/Program.cs
@@ -80,7 +80,7 @@ namespace Mooege
             Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture; // Use invariant culture - we have to set it explicitly for every thread we create to prevent any mpq-reading problems (mostly because of number formats).
 
             string Revision = "48/2"; // Ревизия
-            string date = "18/10/2018";
+            string date = "19/10/2018";
 
             string title = "D3IIIS - Revision [" + Revision + "] - Date [" + date + "] - By AiDiE";
 
